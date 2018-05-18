af632586884b1f254e23085f7874f04de4fa0cb0
ChaitanyaBelsare1691 <chaitanya.belsare@eternussolutions.com>
2018-05-18 15:47:03 +0530

    For UI changes

--- src/PhotoView.js
+++ src/PhotoView.js
@@ -47,8 +47,8 @@ class PhotoView extends Component {
     ));
 
     return (
-      <div className= "row photoDiv">
-        <div className="col-md-6 imgDiv mainImgDiv">
+      <div className= "row photoDiv mainImgBox ">
+        <div className="col-md-6 imgDiv mainImgDiv noPadding">
             <img height="" width="100%" src={this.state.photo.display_src} />
         </div> 
         <div className="col-md-5 userCommentBox">
